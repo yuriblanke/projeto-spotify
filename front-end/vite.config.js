b863bd8b011cb4900b334a543c11df4d7eaf35a7
Yuri Blanke <yuriblanke@gmail.com>
2025-02-24 22:56:33 -0300

    testando outro base

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react()],
   build: {
     target: "esnext",
